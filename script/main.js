8d530cf7e4f149a3fefb3b37baed05f6c2004f70
hwangsujin22 <goddostnwls@naver.com>
2024-03-11 23:59:42 +0900

    2024.03.12 12:00am 업데이트

---
+++ script/main.js
@@ -0,0 +1,16 @@
+// 외부 스크립트
+
+// 1. 검색박스 밑 랜덤 추천 단어
+// let n = 0;
+var list = ['&#x0023;서울', '&#x0023;EDM festival', '&#x0023;제주', '&#x0023;맛집', '&#x0023;친구랑', '&#x0023;봄 축제', '&#x0023;4월 축제', '&#x0023;강원도']
+
+
+
+
+// let ran_n = Math.floor(Math.random()*8);
+// let list_n = list[ran_n];
+
+// document.getElementById('banner_img').src="./images/"+list_n;
+
+
+
