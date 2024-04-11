094b80088197159866d9fc523c7b75c286461d28
hwangsujin22 <goddostnwls@naver.com>
2024-04-11 22:37:37 +0900

    2024.04.11 업데이트

--- script/main.js
+++ script/main.js
@@ -15,19 +15,19 @@ $(document).ready(function(){
 
   // 없는 콘텐츠 알림
   $('#gwangju').click(function(){
-    alert('광주 : 준비된 축제가 없습니다.');
+    alert('준비된 축제가 없습니다.');
     return false;
   });
   $('#ulsan').click(function(){
-    alert('울산 : 준비된 축제가 없습니다.');
+    alert('준비된 축제가 없습니다.');
     return false;
   });
   $('#ulleung').click(function(){
-    alert('울릉도 : 준비된 축제가 없습니다.');
+    alert('준비된 축제가 없습니다.');
     return false;
   });
   $('#dokdo').click(function(){
-    alert('독도 : 준비된 축제가 없습니다.');
+    alert('준비된 축제가 없습니다.');
     return false;
   });
 
@@ -46,14 +46,35 @@ $(document).ready(function(){
 
   let e_Timer = setInterval(fade_slide, 3000);
 
+  // 모바일 버전에서만 토글 클릭 후 화면 보이게
+  let w_width;
+
+  // 브라우저의 크기가 변하면 함수내용을 실행한다.
+  $(window).resize(function(){
+    w_width = $(window).innerWidth();
+    if(w_width>=767){
+      $('.m_gnb_wrap').hide();
+    }
+  }).resize();
 
   // 토글 버튼 클릭시 gnb 노출
   let toggle = $('#toggle');
-  let sub = $('.sub');
   
   toggle.click(function(){
     $('.sub, .sub_back').stop().fadeToggle();
   });
+  $(window).resize(function(){
+    w_width = $(window).innerWidth();
+    if(w_width<=768){
+      $('.sub, .sub_back').hide();
+    }
+  }).resize();
+  $(window).resize(function(){
+    w_width = $(window).innerWidth();
+    if(w_width>=1025){
+      $('.sub, .sub_back').hide();
+    }
+  }).resize();
 
   // 모바일 토글
   let m_toggle = $('#m_toggle');
@@ -69,6 +90,7 @@ $(document).ready(function(){
     $(this).next().slideToggle().parent().siblings().find('.m_sub').hide();
     $('i').removeClass('rotate');
     $(this).find('i').addClass('rotate');
+    $('i').removeClass('rotate');
   });
 
   // 지도 클릭시 배경화면 지정
@@ -82,7 +104,6 @@ $(document).ready(function(){
     $(this).siblings().removeClass('m_act');
     $(this).addClass('m_act');
   });
-
 
   // top버튼 스크롤 위치에 따라 보이기/숨기기----------------------------------
   let scrollPos;
@@ -101,9 +122,6 @@ $(document).ready(function(){
   $('.top_btn_box').click(function(){
     $('html, body').animate({'scrollTop':'0px'},300);
   });
-  
-
-
 
 });
 
