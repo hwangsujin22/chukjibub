6fa782648f63ce605b8fa9e5a55ebde4728a5400
hwangsujin22 <goddostnwls@naver.com>
2024-04-14 22:01:51 +0900

    2024.04.14 축지법 PC 버전으로 재 업데이트

--- script/main.js
+++ script/main.js
@@ -46,65 +46,6 @@ $(document).ready(function(){
 
   let e_Timer = setInterval(fade_slide, 3000);
 
-  // 모바일 버전에서만 토글 클릭 후 화면 보이게
-  let w_width;
-
-  // 브라우저의 크기가 변하면 함수내용을 실행한다.
-  $(window).resize(function(){
-    w_width = $(window).innerWidth();
-    if(w_width>=767){
-      $('.m_gnb_wrap').hide();
-    }
-  }).resize();
-
-  // 토글 버튼 클릭시 gnb 노출
-  let toggle = $('#toggle');
-  
-  toggle.click(function(){
-    $('.sub, .sub_back').stop().fadeToggle();
-  });
-  $(window).resize(function(){
-    w_width = $(window).innerWidth();
-    if(w_width<=768){
-      $('.sub, .sub_back').hide();
-    }
-  }).resize();
-  $(window).resize(function(){
-    w_width = $(window).innerWidth();
-    if(w_width>=1025){
-      $('.sub, .sub_back').hide();
-    }
-  }).resize();
-
-  // 모바일 토글
-  let m_toggle = $('#m_toggle');
-  let m_menu = $('.m_gnb > li:not(:first-child) > a');
-
-  m_toggle.click(function(){
-    $('.m_gnb_wrap').fadeIn();
-    $('.c_btn').click(function(){
-      $('.m_gnb_wrap').fadeOut();
-    });
-  });
-  m_menu.click(function(){
-    $(this).next().slideToggle().parent().siblings().find('.m_sub').hide();
-    $('i').removeClass('rotate');
-    $(this).find('i').addClass('rotate');
-    $('i').removeClass('rotate');
-  });
-
-  // 지도 클릭시 배경화면 지정
-  let m_btn02 = $('.btn02');
-  let m_btn07 = $('.btn07');
-  let m_btn13 = $('.btn13');
-  let m_btn18 = $('.btn18');
-  let m_btn19 = $('.btn19');
-  let m_btn = $('.m_btn');
-  m_btn.click(function(){
-    $(this).siblings().removeClass('m_act');
-    $(this).addClass('m_act');
-  });
-
   // top버튼 스크롤 위치에 따라 보이기/숨기기----------------------------------
   let scrollPos;
   $(window).scroll(function(){
